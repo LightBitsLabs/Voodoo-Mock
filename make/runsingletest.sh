f6e126718429ece2a55281f4302c52be3bdf0eb4
Shimon Turjeman <shimon@lightbitslabs.com>
2018-05-23 16:08:06 +0300

    initial commit

--- make/runsingletest.sh
+++ make/runsingletest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SUITE=$1
 TEST_LINE=$2
+TEST_IDENTIFIER=$2
 
 function isPython {
     echo $SUITE | grep '\.py$' > /dev/null
@@ -10,14 +11,14 @@ function isCxxTest {
 }
 
 if isPython $SUITE; then
-    TEST_NAME=`python $VOODOO_ROOT_DIR/pytest/findtestnamefromlinenumber.py $SUITE $TEST_LINE`
+    TEST_NAME=`python $VOODOO_ROOT_DIR/pytest/gettest.py $SUITE $TEST_IDENTIFIER`
 	python $VOODOO_ROOT_DIR/pytest/pytestrunner.py --verbose --singleTest=$TEST_NAME $SUITE
 else
     if [ ! isCxxTest ]; then
         echo "$SUITE was not recognized as niether python test suite or CxxTest suite files"
         exit 1
     fi
-    TEST_NAME=`python $VOODOO_ROOT_DIR/pytest/findtestnamefromlinenumber.py $SUITE $TEST_LINE`
+    TEST_NAME=`python $VOODOO_ROOT_DIR/pytest/gettest.py $SUITE $TEST_IDENTIFIER`
     NO_EXT=`echo $SUITE | sed 's@\.[hH].\?.\?$@@'`
     NO_EXT_UNDERSCORE=`echo $NO_EXT | sed 's@/@_@g'`
     BIN=build_unittest/${NO_EXT_UNDERSCORE}.bin
--- pytest/gettest.py
+++ pytest/gettest.py
@@ -3,29 +3,36 @@ import re
 
 filename = sys.argv[ 1 ]
 testName = None
+lineNumber = None
+
+with open("blabla.txt", 'w') as fh:
+    fh.write("args = %s" % sys.argv)
 
 lines = open( filename ).readlines()
 
 try:
     lineNumber = int( sys.argv[ 2 ] ) - 1
 except ValueError:
-    testName = sys.argv[2]
-    lineNumber = len(lines)
+    testName = sys.argv[ 2 ]
 
-if not testName:
+if lineNumber:
     assert lineNumber < len( lines ), "Line number must be inside file length"
+
+maxLineNumberToCheck = lineNumber + 1 if lineNumber else len(lines)
 
 PYTHON_TEST_METHOD_DEFINITION = re.compile( "\s+def\s+(test_\w+)\(" )
 CPP_TEST_METHOD_DEFINITION = re.compile( "\s+void\s+(test_\w+)\(" )
 
-for line in reversed( lines[ : lineNumber + 1 ] ):
+for line in reversed( lines[ : maxLineNumberToCheck ] ):
     match = PYTHON_TEST_METHOD_DEFINITION.match( line )
-    if match and testName == match.group(1):
-        print match.group( 1 )
-        sys.exit( 0 )
+    if match:
+        if not testName or testName == match.group(1):
+            print match.group( 1 )
+            sys.exit( 0 )
     match = CPP_TEST_METHOD_DEFINITION.match( line )
-    if match and testName == match.group(1):
-        print match.group( 1 )
-        sys.exit( 0 )
+    if match:
+        if not testName or testName == match.group(1):
+            print match.group( 1 )
+            sys.exit( 0 )
 print "POSITION IN FILE IS NOT INSIDE A TEST METHOD"
 sys.exit( 1 )
